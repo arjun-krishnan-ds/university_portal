a10e79fb3fb9997c481be7652fc95ea5e5024856
Arjun Krishnan <arjunkrishnan818@gmail.com>
2026-01-31 23:10:55 +0530

    Added a superuser creator

--- core/management/commands/create_superuser.py
+++ core/management/commands/create_superuser.py
@@ -1,15 +1,17 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+import os
 
 class Command(BaseCommand):
-    help = "Create admin user if not exists"
+    help = "Create admin user if not exists (permanent, safe)"
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
 
-        username = "admin"
-        email = "admin@example.com"
-        password = "admin12345"
+        # Read credentials from env vars if set
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
         if User.objects.filter(username=username).exists():
             self.stdout.write("Admin already exists")
@@ -21,4 +23,4 @@ class Command(BaseCommand):
             password=password,
         )
 
-        self.stdout.write("Admin user created")
+        self.stdout.write(f"Admin user {username} created successfully")
