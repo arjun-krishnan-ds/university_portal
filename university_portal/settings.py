672a08ff54f7d6a94ad431c876439b0ebe48f52e
Arjun Krishnan <arjunkrishnan818@gmail.com>
2026-02-01 09:35:27 +0530

    Added PostgreSQL DB

--- university_portal/settings.py
+++ university_portal/settings.py
@@ -96,11 +96,11 @@ DATABASES = {
 }
 
 # # Override with PostgreSQL if DATABASE_URL exists
-# DATABASE_URL = os.environ.get("DATABASE_URL")
-# if DATABASE_URL:
-#     DATABASES["default"] = dj_database_url.parse(
-#         DATABASE_URL, conn_max_age=600, ssl_require=True
-#     )
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES["default"] = dj_database_url.parse(
+        DATABASE_URL, conn_max_age=600, ssl_require=True
+    )
 # --------------------------
 # PASSWORD VALIDATION
 # --------------------------
